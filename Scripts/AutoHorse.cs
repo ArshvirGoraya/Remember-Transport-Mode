2ac273b5bd41d5db8e59704b75539c22058ddd84
Arshvir Goraya <113562877+ArshvirGoraya@users.noreply.github.com>
2024-08-24 12:00:49 -0400

    bug fix on exitting ship

--- Scripts/AutoHorse.cs
+++ Scripts/AutoHorse.cs
@@ -12,12 +12,19 @@ using DaggerfallWorkshop.Game;
 using DaggerfallWorkshop.Game.Utility;
 using DaggerfallWorkshop.Game.Utility.ModSupport;
 using DaggerfallWorkshop.Game.Utility.ModSupport.ModSettings;
+using DaggerfallWorkshop.Game.UserInterfaceWindows;
 
 namespace AutoHorseMod
 {
     public class AutoHorse : MonoBehaviour
     {
         TransportManager transportManager;
+        TransportModes storedTransportMode;
+        bool onShip;
+        bool onShipChanged;
+
+        DaggerfallTransportWindow transportWindow;
+
         ModSettings settings;
 
         static Mod mod;
@@ -35,44 +42,75 @@ namespace AutoHorseMod
         }
         void Start(){
             transportManager = GameManager.Instance.TransportManager;
-            transportManager.HandleTransition = PlayerEnterExit_OnPreTransition;
+            storedTransportMode = transportManager.TransportMode;
+            onShip = transportManager.IsOnShip();
+            onShipChanged = false;
+
+            // transportWindow = (DaggerfallTransportWindow)UIWindowFactory.GetInstance(UIWindowType.Transport, DaggerfallUI.Instance.UserInterfaceManager);
+            // Button transportFootButton = transportWindow.footButton;
 
             // Settings:
-            bool rememberTransportMode = settings.GetBool("RememberTransportMode", "RememberTransportMode");
+            // bool rememberTransportMode = settings.GetBool("RememberTransportMode", "RememberTransportMode");
 
             // On Events:
             PlayerEnterExit.OnTransitionExterior += PlayerEnterExit_OnTransitionExterior;
             PlayerEnterExit.OnTransitionDungeonExterior += PlayerEnterExit_OnTransitionExterior;
 
-            PlayerEnterExit.OnTransitionInterior += PlayerEnterExit_OnTransitionInterior;
-            PlayerEnterExit.OnPreTransition += PlayerEnterExit_OnPreTransition;
-
             // if (rememberTransportMode){
-            //     PlayerEnterExit.OnTransitionInterior += PlayerEnterExit_OnTransitionInterior;
             // }
+
+            Debug.Log($"Auto Horse - stored transport mode: {storedTransportMode}"); // TODO: Should set when loading in.
         }
-        ////////////////////////////////////////////////////////////////////////
+
+        void Update(){
+            // * Detect when entering/exiting ship since there is no accessible event for it.
+            if (onShipChanged){
+                onShipChanged = false;
+            }
+            else if (onShip != transportManager.IsOnShip()){
+                onShip = transportManager.IsOnShip();
+                onShipChanged = true;
+            }
+
+            // * Detect when TransportMode has changed: does not include ship for some reason: foot, horse, cart only.
+            if (transportManager.TransportMode != storedTransportMode){
+                changedTransportMode(storedTransportMode, transportManager.TransportMode);
+            }
+        }
+
+        private void changedTransportMode(TransportModes previousTransportMode, TransportModes newTransportMode){
+            // * Store changed transport mode if outside:
+            // * Automatically switches to foot when on ship. Must reset to stored one when entered ship.
+            if (!GameManager.Instance.PlayerEnterExit.IsPlayerInside){ // * Entered ship.
+                if (onShipChanged && onShip){
+                    transportManager.TransportMode = storedTransportMode;
+                    Debug.Log($"Auto horse - Entered Ship. Automatically changed to stored transport value.");
+                    return;
+                } 
+                Debug.Log($"Auto Horse - Detect change changed transport mode outside to: {newTransportMode} from: {previousTransportMode}");
+                storedTransportMode = newTransportMode; 
+            }
+        }
+
         private void PlayerEnterExit_OnTransitionExterior(PlayerEnterExit.TransitionEventArgs args){
-            if (transportManager.HasHorse()){
-                transportManager.TransportMode = TransportModes.Horse;
-            }
-        }
+            // * Entered Exterior
+            // if (transportManager.HasHorse()){
+            //     transportManager.TransportMode = TransportModes.Horse;
+            // }
 
-        private void PlayerEnterExit_OnTransitionInterior(PlayerEnterExit.TransitionEventArgs args){
-            Debug.Log("auto horse - entered interior");
-        }
+            if(onShip){ // * Exited interior of ship. 
+                // * If on Horse or Cart: Must raise the player a bit into the air or will clip through the ship.
+                // if (storedTransportMode == TransportModes.Horse || storedTransportMode == TransportModes.Cart){}
+                GameManager.Instance.PlayerObject.transform.position = new Vector3(
+                    GameManager.Instance.PlayerObject.transform.position.x,
+                    GameManager.Instance.PlayerObject.transform.position.y + 1, 
+                    GameManager.Instance.PlayerObject.transform.position.z
+                    );
 
-        private void PlayerEnterExit_OnPreTransition(PlayerEnterExit.TransitionEventArgs args){
-            if (
-                args.TransitionType == PlayerEnterExit.TransitionType.ToBuildingInterior || 
-                args.TransitionType == PlayerEnterExit.TransitionType.ToDungeonInterior
-            ){
-                Debug.Log($"auto horse - going to interior... with: {transportManager.TransportMode}");
-                // ! Is updating to foot before this... so can't store what TransportMode we are in on entering interiors...
-                // ! Inside of HandleTransition in TransportManager.cs.
-                // ! Alternative way to store TransportMode: when loading into game, get the transport mode.
-                // ! When switching TransportMode's: store the new TransportMode
+                Debug.Log($"Auto horse - Exited ship interior: {GameManager.Instance.PlayerObject.transform.position}");
             }
+            transportManager.TransportMode = storedTransportMode;
+            Debug.Log($"Auto Horse - switch to stored transport mode: {storedTransportMode}");
         }
     }
 }
